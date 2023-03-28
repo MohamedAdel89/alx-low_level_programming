983175c4352685e1dc2889081926a15001a94356
MohamedAdel89 <eng.mooh_adel@yahoo.com>
2023-03-27 17:14:07 -0700

    Pointers

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,18 +4,18 @@
 int password(int password_length) {
 char list[] = "1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*()_- +=QWERTYUIOPASDFGHJKLZXCVBNM[]{};'\"<>:,.?/|";
 printf("\t");
-for(int i = 0; i < password_length; i++) {
+for( i = 0; i < password_length; i++) {
     printf("*");
 }
 printf("\n");
 printf("\t");
 srand(time(NULL));
-for(int i = 0; i < password_length; i++) {
+for( i = 0; i < password_length; i++) {
     printf("%c", list[rand() % (sizeof list - 1)]);
 }
 printf("\n");
 printf("\t");
-for(int i = 0; i < password_length; i++) {
+for( i = 0; i < password_length; i++) {
     printf("*");
 }
 printf("\n");
