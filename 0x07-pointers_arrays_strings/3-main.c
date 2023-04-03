01a471c4a2768c6054f1d5e985f7d7c88d75718f
MohamedAdel89 <eng.mooh_adel@yahoo.com>
2023-04-03 16:37:37 -0700

    First commit for today

---
+++ 0x07-pointers_arrays_strings/3-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	char *s = "hello, world";
+	char *p = "helloIhello";
+	char *f = "oleh";
+	unsigned int n, z;
+
+	n = _strspn(s, f);
+	z = _strspn(p, f);
+	printf("%u\n", n);
+	printf("%u\n", z);
+	return (0);
+}
