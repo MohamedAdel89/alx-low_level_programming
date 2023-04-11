bb977e96bc1a123d29fc58ce683f7b4b5980ce1a
MohamedAdel89 <eng.mooh_adel@yahoo.com>
2023-04-10 19:17:21 -0700

    AVR

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	(void) argc;
-
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 
 	return (0);
 }
