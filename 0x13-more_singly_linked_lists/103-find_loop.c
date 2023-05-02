35467ebd624e38f3cae762ba281fa753e76aa74a
MohamedAdel89 <eng.mooh_adel@yahoo.com>
2023-05-02 11:07:22 -0700

    s

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,41 +1,30 @@
+#include <stdio.h>
 #include "lists.h"
+
 /**
- * @pslow: pointer one by one
- * @pfast: pointer leap one
- * Return: poibter where the loop start
+ *find_listint_loop - finds a loop within a linked list
+ *@head: head of the linked list
+ *Return: beginning of the loop
  */
-listint_t *locateloop(listint_t *pslow, listint_t *pfast)
-{
-	while (pslow != pfast)
-	{
-	pslow = pslow->next;
-	pfast = pfast->next;
-	}
-return (pslow);
-}
-/**
- * find_listint_loop - finds loops inside lists
- * @head: address pointer head
- * Return: number of nodes
- */
+
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *pslow, *pfast;
+	listint_t *turtle = head, *hare = head;
 
-	if (head)
+	while (hare != NULL && hare->next != NULL)
 	{
-		pslow = head->next;
-		pfast = head->next->next;
-		while (pslow && pfast)
+		if (turtle == hare)
 		{
-			if (pslow == pfast)
+			hare = head;
+			while (turtle != hare)
 			{
-				pslow = head;
-				return (locateloop(pslow, pfast));
+				turtle = turtle->next;
+				hare = hare->next;
 			}
-		pslow = pslow->next;
-		pfast = pfast->next->next;
+			break;
 		}
+		turtle = turtle->next;
+		hare = hare->next->next;
 	}
-return (0);
+	return (hare);
 }
