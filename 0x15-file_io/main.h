920471c9657a9ac661ed60fe50f323e1e24c71ad
MohamedAdel89 <eng.mooh_adel@yahoo.com>
2023-05-08 15:33:01 -0700

    adv-elf

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,11 @@
 #ifndef _MAIN_
 #define _MAIN_
-ssize_t read_textfile(const char *filename, size_t letters);
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
 #endif
